534ee802b5c34625b9af3dc6276a89e218892f90
Govind Raj Gupta <gg7078417058@gmail.com>
2025-07-03 23:16:32 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,11 +59,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={[archivoBlack.className].join(" ")}>
       <head>
-        <Script
+        {/* <Script
           defer
           src={process.env.UMAMI_DOMAIN}
           data-website-id={process.env.UMAMI_SITE_ID}
-        ></Script>
+        ></Script> */}
         {/* <Analytics /> */}
       </head>
       <body>
